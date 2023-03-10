7e05cd825a25d95bf547d48b59e215c74c0636e1
TheSimorgh <TheSimurgh30@proton.me>
2023-03-11 04:44:25 +0500

    Cart Finish

--- 3 React-Typescript/lessons/3-ReactTypeScriptCart/src/App.tsx
+++ 3 React-Typescript/lessons/3-ReactTypeScriptCart/src/App.tsx
@@ -1,16 +1,24 @@
-import { useCallback, useEffect, useState, useMemo, useRef } from "react";
+
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Cart from "./components/Cart"
+import ProductList from "./components/ProductList"
+import { useState } from "react"
 
 function App() {
-  useEffect(() => {
-   
-  }, []);
+  const [viewCart, setViewCart] = useState<boolean>(false)
 
+  const pageContent = viewCart ? <Cart /> : <ProductList />
 
-  return (
+  const content = (
     <>
-     1
+      <Header viewCart={viewCart} setViewCart={setViewCart} />
+      {pageContent}
+      <Footer viewCart={viewCart} />
     </>
-  );
+  )
+
+  return content
 }
 
-export default App;
+export default App
